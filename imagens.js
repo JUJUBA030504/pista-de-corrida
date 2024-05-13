6429ab85345ce5b4729002356abf1585f75128d2
JUJUBA030504 <128061411+JUJUBA030504@users.noreply.github.com>
2024-05-13 14:02:36 -0300

    Add files via upload

---
+++ imagens.js
@@ -0,0 +1,15 @@
+let Fundo;
+let imagemCarro;
+let imagemCarro1;
+let imagemCarro2;
+let imagemAtor;
+
+
+ function preload(){
+  Fundo = loadImage("imagens/pistaruim.png");
+  imagemCarro = loadImage("imagens/carros3.png");
+   imagemCarro1 = loadImage("imagens/carros1.png");
+   imagemCarro2 = loadImage("imagens/corrida.png");
+   imagemAtor = loadImage("imagens/urso.png");
+  imagemCarros = [imagemCarro, imagemCarro1, imagemCarro2,imagemCarro, imagemCarro1, imagemCarro2];   
+  }
